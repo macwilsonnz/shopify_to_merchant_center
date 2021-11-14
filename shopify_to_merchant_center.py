a8db58be8ab1ed851f965e2a05a266972bcdc217
Mac Wilson <Wilson.jmac@gmail.com>
2021-11-15 12:12:26 +1300

    Update shopify_to_merchant_center.py
    More changes

--- shopify_to_merchant_center.py
+++ shopify_to_merchant_center.py
@@ -9,8 +9,8 @@ st.title('Shopify products to Google Merchant Center')
 df = pd.DataFrame()
 
 def _removeNonAscii(s):
+    s = str(s)
     return "".join(i for i in s if  ord(i)<128)
-
 
 def process_data(domain_input,quantity_filter,status_filter,include_description_filter):
     try:
@@ -28,20 +28,20 @@ def process_data(domain_input,quantity_filter,status_filter,include_description_
 
       df_filtered = df[(df['Title'].notnull()) & (df['Image Src'].notnull())]
 
-      df_total_inventory = df.groupby(['Handle'], as_index=False)['Variant Inventory Qty'].sum()
-
-      merged_left = pd.merge(left=df_filtered, right=df_total_inventory, how='left', left_on='Handle', right_on='Handle')
-
-      # Select the ones you want
-      df_filtered = merged_left[['Handle','Title','Body (HTML)','Vendor','Published','Variant Price','Image Src','Variant Image','Status','Variant Inventory Qty_y']]
-      df_filtered = df_filtered[(df_filtered['Variant Inventory Qty_y'] >= quantity_filter)]
-
-      if(status_filter):
-        df_filtered = df_filtered[(df_filtered['Status'] == "active")]
-
       df_filtered['Body'] = df_filtered['Body (HTML)'].replace(r'(<.*?>)+',"",regex=True)
-      df_filtered['Body'] =df_filtered['Body'].apply(_removeNonAscii)
+      df_filtered['Body'] = df_filtered['Body'].apply(_removeNonAscii)
 
+      if 'Variant Inventory Qty' in df:
+        df_total_inventory = df.groupby(['Handle'], as_index=False)['Variant Inventory Qty'].sum()
+
+        merged_left = pd.merge(left=df_filtered, right=df_total_inventory, how='left', left_on='Handle', right_on='Handle')
+
+        # Select the ones you want
+        df_filtered = merged_left[['Handle','Title','Body','Vendor','Published','Variant Price','Image Src','Variant Image','Status','Variant Inventory Qty_y']]
+        df_filtered = df_filtered[(df_filtered['Variant Inventory Qty_y'] >= quantity_filter)]
+
+        if(status_filter):
+          df_filtered = df_filtered[(df_filtered['Status'] == "active")]
 
       #Create Categories Dataframe
       df_export_data = pd.DataFrame(columns=["id","title","description","link","condition","price","availability","image_link","gtin","mpn","brand","google product category"])
@@ -56,7 +56,7 @@ def process_data(domain_input,quantity_filter,status_filter,include_description_
                     'price' : '{0:.2f}'.format(row['Variant Price'])+' NZD'
                     }
               if(include_description_filter):
-                df2['description'] = row['Body']
+                df2['description'] = str(row['Body'])
               df_export_data = df_export_data.append(df2, ignore_index = True)
 
       data_load_state = st.text('Completed.')
@@ -67,12 +67,11 @@ def process_data(domain_input,quantity_filter,status_filter,include_description_
 
       # Export Data
       csv = df_export_data.to_csv(index=False)
-      vendor_name = df_export_data.brand.loc[0]
 
       st.download_button(
       "Download",
       csv,
-      vendor_name+"-"+datetime.today().strftime('%Y-%m-%d')+".csv",
+      domain_input+"-"+datetime.today().strftime('%Y-%m-%d')+".csv",
       "text/csv",
       key='download-csv'
       )
@@ -86,12 +85,15 @@ try:
   if uploaded_file is not None:
     data_load_state = st.text('Loading data.')
     df = pd.read_csv(uploaded_file)
-  
+
     data_load_state.text("Successfully imported.")
     st.write(df)
 
     domain_input = st.text_input("Enter the brands domain name(starting with https://)", "https://exampledomain.com",help="This is required to create each of the product links in the export.")
-    quantity_filter = st.slider('Choose the minimum product stock level to include', min_value=0, max_value=100, value=10, step=5, help="Any products with a total stock quantity less than this amount will not be included in the export.")
+
+    quantity_filter = st.slider('Choose the minimum product stock level to include', min_value=0, max_value=100, value=10, step=1, help="Any products with a total stock quantity less than this amount will not be included in the export.")
+    if not 'Variant Inventory Qty' in df:
+      st.text('NOTE: Variant Inventory Qty column does not exist - above stock level filter will not apply')
     status_filter = st.checkbox("Only include active products in export",True, help="If checked, only products with a status of active will be included in the export.")
     include_description_filter = st.checkbox("Include product description in export",True, help="If checked, product description will be included in the export.")
     
